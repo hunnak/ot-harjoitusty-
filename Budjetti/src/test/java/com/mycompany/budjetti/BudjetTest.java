ba7d16412ebd209b0b006e1df8dc91a98faf7dcd
Hunski <joel.hunnakko@helsinki.fi>
2018-11-20 19:58:02 +0200

    monopoly kaavio tehty ja laskari viikko3 kansio luotu

--- Budjetti/src/test/java/com/mycompany/budjetti/BudjetTest.java
+++ Budjetti/src/test/java/com/mycompany/budjetti/BudjetTest.java
@@ -37,9 +37,7 @@ public class BudjetTest {
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    
+     @Test
+     public void hello() {}
 }
