82b994ddbd56965de6e1d237f043efddc116aefa
Hunski <joel.hunnakko@helsinki.fi>
2018-11-20 15:26:14 +0200

    Pääohjelmaan lisätty toiminnallisuutta alustamaan timeandAverage luokkaa/oliota

--- Budjetti/src/main/java/com/mycompany/budjetti/BudjetMain.java
+++ Budjetti/src/main/java/com/mycompany/budjetti/BudjetMain.java
@@ -1,5 +1,7 @@
 package com.mycompany.budjetti;
 
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,11 +14,29 @@ package com.mycompany.budjetti;
  */
 public class BudjetMain {
     public static void main(String[] args){
+        Scanner reader = new Scanner(System.in);
         
-    }
-    public BudjetMain(){
-        System.out.println(" ");
+        int timePeriodInt;
+        while(true){
+            System.out.println("What time period you would like to use?");
+            System.out.println("Choose 1kk, 3kk, 6kk, type 1, 3 or 6");
+            String timePeriod = reader.nextLine();
+            if(timePeriod.equals("1")){
+                timePeriodInt = Integer.parseInt(timePeriod);
+                break;
+            }
+            if(timePeriod.equals("3")){
+                timePeriodInt = Integer.parseInt(timePeriod);
+                break;
+            }
+            if(timePeriod.equals("6")){
+                timePeriodInt = Integer.parseInt(timePeriod);
+                break;
+            }
+            System.out.println("You typed wrong, type 1, 3 or 6");
+        }
+    }    
 }
     
     
-}
+
