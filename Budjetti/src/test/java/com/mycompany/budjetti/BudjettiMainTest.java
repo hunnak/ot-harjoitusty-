198f57f9ca1e9c590008a21b2e2f5fa613368edd
Hunski <joel.hunnakko@helsinki.fi>
2018-11-20 14:42:55 +0200

    korjauksia, ohjelmaa pystyy ajamaan

--- Budjetti/src/test/java/com/mycompany/budjetti/BudjettiMainTest.java
+++ Budjetti/src/test/java/com/mycompany/budjetti/BudjettiMainTest.java
@@ -36,10 +36,7 @@ public class BudjettiMainTest {
     @After
     public void tearDown() {
     }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    
+     @Test
+     public void hello() {}
 }
