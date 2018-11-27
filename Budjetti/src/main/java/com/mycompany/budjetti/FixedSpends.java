3aa172fd197e95152436933bac71efcd4ffd42a7
Hunski <joel.hunnakko@helsinki.fi>
2018-11-27 15:37:41 +0200

    FixedSpends got methods

--- Budjetti/src/main/java/com/mycompany/budjetti/FixedSpends.java
+++ Budjetti/src/main/java/com/mycompany/budjetti/FixedSpends.java
@@ -10,6 +10,45 @@ package com.mycompany.budjetti;
  *
  * @author joelhunn
  */
+/*
+Fixed spends rent, mininum spend on food, hygienia, buscard, car payment & gas, loan payments, stabile hobbies,
+stabile student organization fees. 
+*/
 public class FixedSpends {
+    
+        private int totalspend;
+    public FixedSpends(){
+        
+    }
+    public void setTotalSpends(int x){
+        this.totalspend=x;
+    }
+    public int getSpends(){
+        return this.totalspend;
+    }
+    public void setRent(int x){
+        this.totalspend=this.totalspend+x;
+    }
+    public void setMinFood(int x){
+        this.totalspend=this.totalspend+x;
+    }
+    public void setHyg(int x){
+        this.totalspend=this.totalspend+x;
+    }
+    public void setLoanPay(int x){
+        this.totalspend=this.totalspend+x;
+    }
+    public void setHob(int x){
+        this.totalspend=this.totalspend+x;
+    }
+    public void setStuOrgFe(int x){
+        this.totalspend=this.totalspend+x;
+    }
+    public void setBuscard(int x){
+        this.totalspend=this.totalspend+x;
+    }
+    public void setCar(int x){
+        this.totalspend=this.totalspend+x;
+    }
     
 }
